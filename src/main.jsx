79da3a2a4ed36b73af5c4f6bc431a2aacd6eeb54
Your Name <your_email@example.com>
2025-11-12 19:45:44 +0530

    Deploy Task_Mpt app

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/LifeStyle-and-Wellness-Blog">
+  <BrowserRouter basename="/Task-Mpt">
     <App />
   </BrowserRouter>
 );
